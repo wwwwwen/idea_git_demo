6e954b3fbfccfa297d77002bc8415e4aa016f1de
xu <123>
2022-11-13 15:11:26 +0800

    二次提交

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,5 +9,25 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new Thread(() -> {
+            for (int i=0;i<10;i++){
+                System.out.println("线程1");
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        new Thread(() -> {
+            for (int i=0;i<10;i++){
+                System.out.println("线程2");
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
