b8daed82f0b078978536df4cbcd6b180ca7e9c23
wwwwwen <96176686+wwwwwen@users.noreply.github.com>
2022-11-14 14:35:44 +0800

    origin main add

--- src/main/java/org/example/App2.java
+++ src/main/java/org/example/App2.java
@@ -3,5 +3,6 @@ package org.example;
 public class App2 {
   public static void main(String[] args){
     System.out.println("github remote origin main changed the file");
+    System.out.println("origin main add");
   }
 }
